aea860779cdbc6ec7705c24873fb0b9616181200
1nsekhon <63224811+1nsekhon@users.noreply.github.com>
2020-10-02 16:15:51 -0700

    Assignment 2

---
+++ movieApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  movieApp
+//
+//  Created by Nanki Sekhon on 10/2/20.
+//  Copyright © 2020 Nanki Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
